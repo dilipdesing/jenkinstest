b1a8e53dac067ed07015fbf055a0b16161a76e82
dilipdesing <dilipdesing@gmail.com>
2024-07-25 20:48:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,8 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      # version = "5.17.0"
     }
   }
-  # backend "s3" {
-  #   bucket = "testcloudinstitution"
-  #   key    = "state/terraform.tfstate"
-  #   region = "us-east-1"
-  # }
 }
 
 provider "aws" {
