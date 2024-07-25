3dd8725cc1e63a149c0184366d7efeb00e185869
dilipdesing <dilipdesing@gmail.com>
2024-07-25 20:22:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "1.6.4"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "5.17.0"
+    }
+  }
+  # backend "s3" {
+  #   bucket = "testcloudinstitution"
+  #   key    = "state/terraform.tfstate"
+  #   region = "us-east-1"
+  # }
+}
+
+
+resource "aws_iam_user" "for_each" {
+  for_each = toset(["aaaaa", "bbbbb", "cccccc", "dddddd"])
+  name     = each.key
+  path     = "/"
+}
